61f2bda10e84d97da1ccbe877b45b5ce8635ec70
Raksha Raut <raksharaut174@gmail.com>
2024-08-07 15:00:55 +0545

    Initial setup of React Native app with basic UI components

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        <Text style={{color: "green",fontSize: 30, fontFamily: "poppins", fontWeight: "bold"}}>Hola! Buenas Dias </Text>
+      </Text>
+      <Image 
+        source={require('./assets/sunny.jpg')} 
+        style={{ width: 300, height: 300 }}
+      />
+      <Image 
+        source={{ uri: "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" }} // Ensure the base64 string is valid and complete.
+        style={{ width: wp("80%") , height: 200 }}
+      />
+      <Text>
+        <Text style={{color: "green", fontSize: 30}}>que tenga un buen día</Text>
+      </Text>
+      <Text>
+        <Text style={{color: "green", fontSize: 30}}>que tenga un buen día</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
